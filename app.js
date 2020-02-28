a100d2dca66f2c414634fff8111ac1fdbfd11c0e
Aaron P Foree <af29173@srcs.loc>
2020-02-28 13:19:39 -0800

    fuctions working

--- app.js
+++ app.js
@@ -1,76 +1,117 @@
+/*pounds to kilograms
+pounds to ounces
+pounds to grams
+kilograms to ounces
+kilograms to grams
+ounces to grams */
 function lb_kg(x) {
-    var fnum = x / 2.2046226218;
-    return fnum;
+  var fnum = x / 2.2046226218;
+  return fnum;
 }
 
 function lb_g(x) {
-    var fnum = lb_kg(x) * 1000;
-    return fnum;
+  var fnum = lb_kg(x) * 1000;
+  return fnum;
 }
 
 function lb_oz(x) {
-    var fnum = x * 16;
-    return fnum;
+  var fnum = x * 16;
+  return fnum;
 }
 
 function oz_kg(x) {
-    var fnum = lb_kg(x) / 16;
-    return fnum;
+  var fnum = lb_kg(x) / 16;
+  return fnum;
 }
 
 function oz_g(x) {
-    var fnum = lb_kg(x) / 16 * 1000;
-    return fnum;
+  var fnum = (lb_kg(x) / 16) * 1000;
+  return fnum;
 }
 
 function oz_lb(x) {
-    var fnum = x / 16;
-    return fnum;
+  var fnum = x / 16;
+  return fnum;
 }
 
 function kg_lb(x) {
-    var fnum = x * 2.2046226218;
-    return fnum;
+  var fnum = x * 2.2046226218;
+  return fnum;
 }
 
 function kg_oz(x) {
-    var fnum = kg_lb(x) * 16;
-    return fnum;
+  var fnum = kg_lb(x) * 16;
+  return fnum;
 }
 
 function kg_g(x) {
-    var fnum = x * 1000;
-    return fnum;
+  var fnum = x * 1000;
+  return fnum;
 }
 
 function g_lb(x) {
-    var fnum = kg_lb(x) / 1000;
-    return fnum;
+  var fnum = kg_lb(x) / 1000;
+  return fnum;
 }
 
 function g_oz(x) {
-    var fnum = kg_lb(x) / 1000 * 16;
-    return fnum;
+  var fnum = (kg_lb(x) / 1000) * 16;
+  return fnum;
 }
 
 function g_kg(x) {
-    var fnum = x / 1000;
-    return fnum;
-}
-
-
-function convert(x) {
-    x = document.getElementById("input").value;
-    var 1 st = document.getElementById("unit-select1").selectedIndex;
-    var 2n d = document.getElementById("unit-select2").selectedIndex;
-    if (1 st == "lb" && 2n d == "kg") {
-        lb_kg(x);
-        var number = document.createElement("p");
-        number.textContent = fnum;
-    }
+  var fnum = x / 1000;
+  return fnum;
 }
 
 let convertB = document.getElementById("convert");
 convertB.addEventListener("click", () => {
-    convert();
+  convert();
 });
+
+function convert(x) {
+  x = document.getElementById("input").value;
+  var i = document.getElementById("unit-select1").value;
+  var o = document.getElementById("unit-select2").value;
+  var number = document.createElement("p");
+
+  if (i == "lb" && o == "kg") {
+    var c = lb_kg(x);
+  }
+  if (i == "lb" && o == "g") {
+    var c = lb_g(x);
+  }
+  if (i == "lb" && o == "oz") {
+    var c = lb_oz(x);
+  }
+  if (i == "oz" && o == "kg") {
+    var c = oz_kg(x);
+  }
+  if (i == "oz" && o == "g") {
+    var c = oz_g(x);
+  }
+  if (i == "oz" && o == "lb") {
+    var c = oz_lb(x);
+  }
+  if (i == "kg" && o == "lb") {
+    var c = kg_lb(x);
+  }
+  if (i == "kg" && o == "oz") {
+    var c = kg_oz(x);
+  }
+  if (i == "g" && o == "g") {
+    var c = kg_g(x);
+  }
+  if (i == "g" && o == "lb") {
+    var c = g_lb(x);
+  }
+  if (i == "g" && o == "oz") {
+    var c = g_oz(x);
+  }
+  if (i == "g" && o == "kg") {
+    var c = g_kg(x);
+  }
+
+  number.textContent = c + " " + o + ".";
+  document.body.appendChild(number);
+}
